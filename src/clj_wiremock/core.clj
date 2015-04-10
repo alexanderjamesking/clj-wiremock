19518834ccc838937899b2338526853a2e95322e
Alex King <alex@alexanderjamesking.com>
2015-04-10 04:47:07 +0100

    started wiremock, reset inbetween tests, wrapped a few methods

--- src/clj_wiremock/core.clj
+++ src/clj_wiremock/core.clj
@@ -1,13 +1,25 @@
 (ns clj-wiremock.core
+  (:require [cheshire.core :refer :all])
   (:import com.github.tomakehurst.wiremock.WireMockServer
            com.github.tomakehurst.wiremock.core.WireMockConfiguration
            com.github.tomakehurst.wiremock.client.WireMock))
 
+(defn listAllStubMappings [] (WireMock/listAllStubMappings))
+(defn get-all-mappings []
+  (parse-string (.toString (.getMappings (listAllStubMappings))) true))
+
+(defn reset-mappings [wiremock-server] (.resetMappings wiremock-server))
+(defn start [wiremock-server] (.start wiremock-server))
+(defn stop [wiremock-server] (.stop wiremock-server))
+
 (defn GET [x] (WireMock/get x))
 
-(defn urlEqualTo [url] (WireMock/urlEqualTo url))
-(defn urlMatching [url] (WireMock/urlMatching url))
-(defn urlPathEqualTo [url] (WireMock/urlPathEqualTo url))
-(defn stubFor [x] (WireMock/stubFor x))
-(defn response [] (WireMock/aResponse))
+(defn url-equal-to [url] (WireMock/urlEqualTo url))
+(defn url-matching [url] (WireMock/urlMatching url))
+(defn url-path-equal-to [url] (WireMock/urlPathEqualTo url))
+(defn stub-for [x] (WireMock/stubFor x))
+(defn create-response [] (WireMock/aResponse))
 
+(defn will-return [req res] (.willReturn req res))
+(defn with-body [response-builder body] (.withBody response-builder body))
+(defn with-status [response-builder status] (.withStatus response-builder status))
--- test/clj_wiremock/core_test.clj
+++ test/clj_wiremock/core_test.clj
@@ -1,18 +1,49 @@
 (ns clj-wiremock.core-test
   (:require [clojure.test :refer :all]
             [clj-wiremock.core :refer :all]
-            [cheshire.core :refer :all]))
+            [cheshire.core :refer :all])
+  (:import com.github.tomakehurst.wiremock.WireMockServer
+           com.github.tomakehurst.wiremock.core.WireMockConfiguration
+           com.github.tomakehurst.wiremock.client.WireMock))
+
+(def wiremock-config (.port (new WireMockConfiguration) 22222))
+(def wiremock-server (new com.github.tomakehurst.wiremock.WireMockServer wiremock-config))
+ 
+(use-fixtures :once 
+  (fn [f]
+    (WireMock/configureFor "localhost" 22222)
+    (start wiremock-server)
+    (f)
+    (stop wiremock-server)))
+ 
+(use-fixtures :each 
+  (fn [f]
+    (reset-mappings wiremock-server)
+    (f)))
 
 (defn mapping [req res]
   (parse-string (.toString (.build (.willReturn req res))) true))
 
-(deftest mapping-builder
+(deftest mappings-are-built-correctly
   (testing "wrapped methods create a mapping builder"
-    (let [m (mapping (GET (urlEqualTo "/hello")) 
-                     (-> (response)
-                         (.withBody "Hello World")
-                         (.withStatus 200)))]
+    (let [m (mapping (GET (url-equal-to "/hello")) 
+                     (-> (create-response)
+                         (with-body "Hello World")
+                         (with-status 200)))]
       (is (= "/hello" (-> m :request :url)))
       (is (= "GET" (-> m :request :method)))
       (is (= 200 (-> m :response :status)))
       (is (= "Hello World" (-> m :response :body))))))
+
+(deftest mappings-posted-to-wiremock
+  (testing "mappings are posted to wiremock successfully"
+    (stub-for (will-return (GET (url-equal-to "/hello-world")) 
+                           (-> (create-response) 
+                               (with-body "Hello World")
+                               (with-status 200))))
+    (let [m (first (get-all-mappings))]
+      (is (= "/hello-world" (-> m :request :url)))
+      (is (= "GET" (-> m :request :method)))
+      (is (= 200 (-> m :response :status)))
+      (is (= "Hello World" (-> m :response :body))))))
+
