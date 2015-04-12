dcd8e66e454732c2b8f8888d82d6694e82b3d0db
Alex King <alex@alexanderjamesking.com>
2015-04-12 17:56:12 +0100

    bumped version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-wiremock "0.2.0"
+(defproject clj-wiremock "0.3.0"
   :description "A clojure wrapper around wiremock"
   :url "https://github.com/alexanderjamesking/clj-wiremock"
   :license {:name "Eclipse Public License"
