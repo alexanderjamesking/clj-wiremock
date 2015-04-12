763c3b6c289b838717f38b25754dd088973106a9
Alex King <alex@alexanderjamesking.com>
2015-04-12 17:54:39 +0100

    updated dependencies

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.github.tomakehurst/wiremock "1.54"]
-                 [cheshire "5.4.0"]]
-  :profiles {:dev {:dependencies [[clj-http "1.1.0"]]}})
+                 [cheshire "5.4.0"]
+                 [clj-http "1.1.0"]])
