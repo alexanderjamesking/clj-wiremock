db65525cbb311376cf6021ae9e78c7520c815e01
Alex King <alex@alexanderjamesking.com>
2017-02-17 19:30:14 +0000

    bumped clojure version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/alexanderjamesking/clj-wiremock"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.github.tomakehurst/wiremock "2.5.1"]
                  [cheshire "5.7.0"]
                  [clj-http "3.4.1"]])
